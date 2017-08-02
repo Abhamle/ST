3bb3573dfaca4b6846436a63993859b13e450ef4
A M <abelmengistu2000@gmail.com>
2017-08-02 12:26:01 -0400

    master: created the ORA web application project inside the repository

---
+++ ORA/ORA/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ORA.Startup))]
+namespace ORA
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
