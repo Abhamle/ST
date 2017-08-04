6b873853951efa7e7464858354a1ce28b7765cb9
A M <abelmengistu2000@gmail.com>
2017-08-04 11:01:25 -0400

    Work in progress

--- ORA/ORA/App_Start/RouteConfig.cs
+++ ORA/ORA/App_Start/RouteConfig.cs
@@ -13,6 +13,10 @@ namespace ORA
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
+            routes.MapRoute("Cuisine",
+                "Cuisine/{name}",
+               new { controller = "Cuisine", action = "Search", name = "" });
+
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
